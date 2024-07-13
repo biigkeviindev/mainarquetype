b39a495b5f3a5a4a32e4397ac5443b8be91ffded
fullkevinriveradev <kevinrivera.helloworld@gmail.com>
2024-07-13 12:28:41 +0200

    feat: update footer config

--- packages/Footer/Footer.tsx
+++ packages/Footer/Footer.tsx
@@ -6,15 +6,42 @@ const Footer = ({ links = [] }: any) => {
   return (
     <Layout
       children={
-        <section className=" bg-black text-white">
-          <div className="pt-10 pb-10 px-7">
-            <div>
-              {links.map((mainSection: any, key: number) => (
-                <FooterLinkItem mainSection={mainSection} />
-              ))}
+        <>
+          <section className=" bg-black text-white">
+            <div className="pt-10 pb-10 px-7">
+              <div>
+                {links.map((mainSection: any, key: number) => (
+                  <FooterLinkItem key={key} mainSection={mainSection} />
+                ))}
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section className="flex justify-center border-[#f1f2f3] border-2 gap-7 py-4">
+            <img
+              src="/new-facebook.svg"
+              className="w-[20px] h-[20px]"
+              alt="Enlace facebook"
+            />
+            <img
+              src="/new-linkedin.svg"
+              className="w-[20px] h-[20px]"
+              alt="Enlace linkedIn"
+            />
+            <img
+              src="/new-instagram.svg"
+              className="w-[20px] h-[20px]"
+              alt="Enlace instagram"
+            />
+          </section>
+          <section className=" bg-black text-white">
+            <div className="pt-10 pb-10 px-7 text-center fotn-bold">
+              <span className="text-[22px]">PIXELCODE</span>
+              <p className="text-[13px] text-[#c5c5c7]">
+                © 2010 - 2024 CodeAi Europe. Todos los derechos reservados.
+              </p>
+            </div>
+          </section>
+        </>
       }
     />
   );
